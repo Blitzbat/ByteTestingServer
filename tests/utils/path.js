a6ca75bd172a9a03e6e4f548b61041832a5dd58b
Sebastian Parwolowski <sebastian@bitwantsbyte.com>
2016-02-20 21:59:25 +0100

    Updated tests for path util

--- tests/utils/path.js
+++ tests/utils/path.js
@@ -7,9 +7,9 @@ describe("PathUtil", () => {
     var SESSIONID = "123456";
 
     var timeout;
-    
+
     describe("path functions", () => {
-       it('should return correct session path', () => {
+        it('should return correct session path', () => {
             var path = pathUtil.getSessionPath(SESSIONID);
             expect(path).toBe(process.cwd() + '/sessions/' + SESSIONID);
         });
@@ -22,35 +22,89 @@ describe("PathUtil", () => {
         it('should return correct spec path', () => {
             var path = pathUtil.getSpecPath(SESSIONID)
             expect(path).toBe(process.cwd() + '/sessions/' + SESSIONID + "/specs");
-        }); 
+        });
     });
-    
+
+    describe("specs", () => {
+
+        beforeEach((done) => {
+            pathUtil
+                .createBaseDirectory()
+                .then(() => {
+                    pathUtil.createSessionDirectory(SESSIONID)
+                })
+                .then(() => {
+                    pathUtil.createSpecDirectory(SESSIONID)
+                }).then(() => {
+                    fs.writeFileSync(pathUtil.getSpecPath(SESSIONID) + "/spec1.js", "spec1");
+                    fs.writeFileSync(pathUtil.getSpecPath(SESSIONID) + "/spec2.js", "spec2");
+                    done();
+                });
+
+        });
+
+        it("should get specs", (done) => {
+            pathUtil.getSpecs(SESSIONID).then((files) => {
+                expect(files).toBeDefined();
+                expect(files.length).toBe(2);
+                done();
+            });
+        });
+        
+        it("should extract spec files", (done) => {
+           var zipObj = {
+                files: {
+                    test1: {
+                        asNodeBuffer: function() {
+                            return new Buffer("test1234");
+                        }
+                    }
+                }
+            };
+            
+            pathUtil.extractSpecs(SESSIONID, zipObj);
+            
+            fs.exists(pathUtil.getSpecPath(SESSIONID)+"/test1", (exists) => {
+                expect(exists).toBeTruthy();
+                done();
+            });
+
+        });
+
+        afterEach((done) => {
+            rimraf(process.cwd() + '/sessions', () => {
+                done();
+            });
+        });
+
+    });
+
     describe("where base directory does not exist", () => {
-        it("should fail on creating session directory", (done)=>{
-            pathUtil.createSessionDirectory(SESSIONID).then(() => {                
-            },(err) => {
+        it("should fail on creating session directory", (done) => {
+            pathUtil.createSessionDirectory(SESSIONID).then(() => {
+            }, (err) => {
                 expect(err).toBeDefined()
                 done();
             });
         });
-        
-        it("should fail on creating result directory", (done)=>{
-            pathUtil.createResultDirectory(SESSIONID).then(() => {                
-            },(err) => {
+
+        it("should fail on creating result directory", (done) => {
+            pathUtil.createResultDirectory(SESSIONID).then(() => {
+            }, (err) => {
                 expect(err).toBeDefined()
                 done();
             });
         });
-        
-        it("should fail on creating spec directory", (done)=>{
-            pathUtil.createSpecDirectory(SESSIONID).then(() => {                
-            },(err) => {
+
+        it("should fail on creating spec directory", (done) => {
+            pathUtil.createSpecDirectory(SESSIONID).then(() => {
+            }, (err) => {
                 expect(err).toBeDefined()
                 done();
             });
         });
     });
-    
+
     describe("where base directory is exists", () => {
         beforeEach((done) => {
             timeout = jasmine.DEFAULT_TIMEOUT_INTERVAL;
@@ -61,7 +115,7 @@ describe("PathUtil", () => {
             });
             done();
         });
-      
+
         it("should create session directory", (done) => {
             pathUtil.createSessionDirectory(SESSIONID).then(() => {
                 fs.exists(pathUtil.getSessionPath(SESSIONID), (exsists) => {
